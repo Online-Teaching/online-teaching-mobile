c8a24675d174ce8a1349deaf3de7744d1764bf4d
edaersu <edaersu1@gmail.com>
2020-10-24 13:58:15 +0300

    added splash screen

---
+++ lib/app/view/test_page/test_view.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:online_teaching_mobile/app/view/test_page/test_view_model.dart';
+
+class TestView extends TestViewModel {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text("vbnmöç"),
+      ),
+    );
+  }
+}
