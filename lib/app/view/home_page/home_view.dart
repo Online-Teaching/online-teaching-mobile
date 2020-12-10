83d1cd66c87e42cc7391ee71c747eb56cfbe9b12
edaersu <edaersu1@gmail.com>
2020-12-10 17:28:45 +0300

    data search

--- lib/app/view/home_page/home_view.dart
+++ lib/app/view/home_page/home_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:logger/logger.dart';
+import 'package:online_teaching_mobile/app/model/subject_model.dart';
 import 'package:online_teaching_mobile/app/service/api/apiUrl.dart';
 import 'package:online_teaching_mobile/app/view_model/home_view_model.dart';
 import 'package:online_teaching_mobile/core/component/appbar_with_stack.dart';
@@ -13,8 +14,10 @@ class HomeView extends HomeViewModel {
   @override
   Icon icon = Icon(Icons.bookmark_border);
   int _selectedIndex;
+  Subject gotothisSubject;
   @override
   Widget build(BuildContext context) {
+    getSubjects();
     logger.i("build");
     Size size = MediaQuery.of(context).size;
     return Container(
@@ -81,22 +84,22 @@ class HomeView extends HomeViewModel {
               child: Row(
                 children: <Widget>[
                   Expanded(
-                    child: TextField(
-                      onChanged: (value) {},
-                      decoration: InputDecoration(
-                        hintText: "Search",
-                        hintStyle: TextStyle(
-                          color: Colors.black.withOpacity(0.5),
-                        ),
-                        enabledBorder: InputBorder.none,
-                        focusedBorder: InputBorder.none,
-                      ),
-                      onTap: () {
-                        setState(() {});
-                      },
+                    child: Container(
+                      child: Text("ffff"),
                     ),
                   ),
-                  Icon(Icons.search)
+                  IconButton(
+                    icon: Icon(Icons.search),
+                    color: Colors.black,
+                    onPressed: () {
+                      this.setState(() {
+                        showSearch(
+                            context: context,
+                            delegate: DataSearch(
+                                subjects, subjects_string, navigation));
+                      });
+                    },
+                  )
                 ],
               ),
             ),
@@ -119,4 +122,107 @@ class HomeView extends HomeViewModel {
           });
         },
       ));
+}
+
+class DataSearch extends SearchDelegate<String> {
+  final List<Subject> subjects_list;
+  final List<String> subjects_name_list;
+  var navigation;
+
+  DataSearch(this.subjects_list, this.subjects_name_list, this.navigation);
+
+  final recentCategories = [
+    "",
+  ];
+  @override
+  List<Widget> buildActions(BuildContext context) {
+    return [
+      IconButton(
+        icon: Icon(Icons.clear),
+        onPressed: () {
+          query = "";
+        },
+      )
+    ];
+  }
+
+  @override
+  Widget buildLeading(BuildContext context) {
+    return IconButton(
+      icon: AnimatedIcon(
+          icon: AnimatedIcons.menu_arrow, progress: transitionAnimation),
+      onPressed: () {
+        close(context, null);
+      },
+    );
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {}
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    final suggestionList = query.isEmpty
+        ? recentCategories
+        : subjects_name_list
+            .where((p) => p.toUpperCase().contains(query.toUpperCase()))
+            .toList();
+
+    return ListView.builder(
+      itemBuilder: (context, index) => ListTile(
+          onTap: () {
+            close(context, null);
+            findkonuid(suggestionList[index]);
+          },
+          title: RichText(
+              text: TextSpan(
+                  text: suggestionList[index].substring(0, query.length),
+                  style:
+                      context.textTheme.headline6.copyWith(color: Colors.black),
+                  children: [
+                TextSpan(
+                  text: suggestionList[index].substring(query.length),
+                  style:
+                      context.textTheme.headline6.copyWith(color: Colors.grey),
+                )
+              ]))),
+      itemCount: suggestionList.length,
+    );
+  }
+
+  findkonuid(String id) {
+    Subject gotothisSubject;
+    for (var item in subjects_list) {
+      if (id == item.title) {
+        gotothisSubject = item;
+        create_Url(item.id);
+        navigation.navigateToPage(path: NavigationConstants.DETAIL_VIEW);
+      }
+    }
+  }
+
+  void create_Url(String id) {
+    String category_url = "";
+    String sub_category_url = "";
+    id.runes.forEach((int rune) {
+      if (isIntNumber(String.fromCharCode(rune))) {
+        sub_category_url += String.fromCharCode(rune);
+      } else {
+        category_url += String.fromCharCode(rune);
+      }
+    });
+
+    api_category_url = category_url;
+    api_sub_category_index = sub_category_url;
+    //  getSingleCategory();
+  }
+}
+
+isIntNumber(String char) {
+  List<String> numberList = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  if (numberList.contains(char)) {
+    return true;
+  } else {
+    return false;
+  }
 }
